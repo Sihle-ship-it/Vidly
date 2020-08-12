7aae888c1578b18044bd341c3ffd2017169ff43f
Sihle-ship-it <innocentsihle29@gmail.com>
2020-08-11 22:13:54 -0500

    Added birthdate to customer

---
+++ Vidly/Migrations/202008120213474_AddedDateToCustomer.cs
@@ -0,0 +1,18 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedDateToCustomer : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Customers", "Date", c => c.DateTime());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Customers", "Date");
+        }
+    }
+}

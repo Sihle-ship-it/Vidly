9d471c22370df34d7d9b5aeb6cac33abe6d3034f
Sihle-ship-it <innocentsihle29@gmail.com>
2020-09-01 10:13:32 -0500

    Add a form to create/add movie

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -9,14 +9,23 @@ namespace Vidly.Models
     public class Movie
     {
         public int Id { get; set; }
+        
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+        
         public DateTime DateAdded { get; set; }
+        
+        [Display(Name = "Release Date")]
         public DateTime DateReleased { get; set; }
+        
+        [Display(Name = "Number in Stock")]
         public int InStock { get; set; }
-        [Required]
+
         public Genre Genre { get; set; }
+        
+        [Required]
+        [Display(Name = "Genre")]
         public byte GenreId { get; set; }
     }
 }
